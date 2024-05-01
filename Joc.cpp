eb86f6e099d397a465c32f0de568977d426181d7
NIU1707523 <email@example.com>
2024-05-01 23:14:58 +0200

    Commit_joc.h

--- Joc.cpp
+++ Joc.cpp
@@ -1,1 +1,39 @@
+#include "Joc.h"
 
+void Joc::inicialitza(const string& nomFitxer)
+{
+	ifstream fitxer;
+	fitxer.open(nomFitxer);
+	if (fitxer.is_open())
+	{
+		int fil, col, gir, tip;
+		fitxer >> tip >> fil >> col >> gir;
+		m_Figura_a.Inicialitza_Figura_Tipus(tip, col, fil);
+
+		for (int i = 0; i < length; i++)
+		{
+			m_Figura_a.GirarFigura(GIR_HORARI);
+		}
+
+	}
+}
+
+bool Joc::giraFigura(DireccioGir direccio)
+{
+
+}
+
+bool Joc::mouFigura(int direccio)
+{
+
+}
+
+int Joc::baixaFigura()
+{
+
+}
+
+void Joc::escriuTauler(const string& nomFitxer)
+{
+
+}
--- Joc.h
+++ Joc.h
@@ -1,1 +1,23 @@
+#ifndef JOC_H
+#define JOC_H
+#include "Tauler.h"
+#include "Figura.h"
+#include <string>
+using namespace std;
 
+class Joc
+{
+public:
+	Joc() {}
+	void inicialitza(const string& nomFitxer);
+	bool giraFigura(DireccioGir direccio);
+	bool mouFigura(int dirX);
+	int baixaFigura();
+	void escriuTauler(const string& nomFitxer);
+
+private:
+	Tauler m_Tauler;
+	Figura m_Figura_a;
+};
+
+#endif
