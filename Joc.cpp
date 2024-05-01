88481d571fcd6f4fc1977d471b499da620b20352
NIU1707523 <email@example.com>
2024-05-02 01:00:07 +0200

    Joc.Commit

--- Joc.cpp
+++ Joc.cpp
@@ -1,92 +1,93 @@
 #include "Joc.h"
+#include <fstream>
 
 void Joc::inicialitza(const string& nomFitxer)
 {
-	ifstream fitxer;
-	fitxer.open(nomFitxer);
-	if (fitxer.is_open())
-	{
-		int fil, col, gir, tip;
-		fitxer >> tip >> fil >> col >> gir;
-		m_Figura_a.Inicialitza_Figura_Tipus(tip, col, fil);
+    ifstream fitxer;
+    fitxer.open(nomFitxer);
+    if (fitxer.is_open())
+    {
+        int fil, col, gir, tip;
+        fitxer >> tip >> fil >> col >> gir;
+        m_Figura_a.Inicialitza_Figura_Tipus(static_cast<TipusFigura>(tip), col, fil);
 
-		for (int i = 0; i < length; i++)
-		{
-			m_Figura_a.GirarFigura(GIR_HORARI);
-		}
+        for (int i = 0; i < gir; i++)
+        {
+            m_Figura_a.GirarFigura(GIR_HORARI);
+        }
 
-		int taulerInicial[MAX_FILA][MAX_COL];
-		int n_color;
-		for (int i = 0; i < MAX_FILA; i++)
-			for (int j = 0; j < MAX_COL; j++)
-			{
-				fitxer >> n_color;
-				taulerInicial[i][j] = n_color;
-			}
-		m_tauler.inicialitza(taulerInicial);
-		fitxer.close();
-	}
+        int taulerInicial[MAX_FILA][MAX_COL];
+        int n_color;
+        for (int i = 0; i < MAX_FILA; i++)
+            for (int j = 0; j < MAX_COL; j++)
+            {
+                fitxer >> n_color;
+                taulerInicial[i][j] = n_color;
+            }
+        m_Tauler.inicialitza(taulerInicial);
+        fitxer.close();
+    }
 }
 
 bool Joc::giraFigura(DireccioGir direccio)
 {
-	m_Figura_a.GirarFigura(direccio);
+    m_Figura_a.GirarFigura(direccio);
 
-	bool girValid = m_tauler.girValid(m_Figura_a);
-	if (girValid)
-	{
-		if (direccio == GIR_HORARI)
-			direccio = GIR_ANTI_HORARI;
-		else
-			direccio = GIR_HORARI;
-		m_Figura_a.GirarFigura(direccio);
-	}
-	return !girValid;
+    bool girValid = m_Tauler.girValid(m_Figura_a);
+    if (!girValid)
+    {
+        if (direccio == GIR_HORARI)
+            direccio = GIR_ANTI_HORARI;
+        else
+            direccio = GIR_HORARI;
+        m_Figura_a.GirarFigura(direccio);
+    }
+    return girValid;
 }
 
 bool Joc::mouFigura(int direccio)
 {
-	m_Figura_a.mou(direccio);
-	bool girValid = m_tauler.girValid(m_Figura_a);
-	if (girValid)
-		m_Figura_a.mouFigura(-dirX);
-	return !girValid;
+    m_Figura_a.mouFigura(direccio);
+    bool movimentValid = m_Tauler.movimentValid(m_Figura_a);
+    if (!movimentValid)
+        m_Figura_a.mouFigura(-direccio);
+    return movimentValid;
 }
 
 int Joc::baixaFigura()
 {
-	int nFiles = 0;
-	m_Figura_a.mou.BaixaFigura();
-	if (m_tauler.girValid(m_Figura_a))
-	{
-		m_Figura_a.PujaFigura();
-		nFiles = m_tauler.colocaFigura(m_Figura_a);
-		m_Figura_a.inicialitza(NO_FIGURA, -1, -1);
-	}
-	return nFiles;
+    int nFiles = 0;
+    m_Figura_a.BaixaFigura();
+    if (!m_Tauler.movimentValid(m_Figura_a))
+    {
+        m_Figura_a.PujaFigura();
+        nFiles = m_Tauler.colocaFigura(m_Figura_a);
+        m_Figura_a.Inicialitza_Figura(NO_FIGURA);
+    }
+    return nFiles;
 }
 
 void Joc::escriuTauler(const string& nomFitxer)
 {
-	ofstream fitxer;
-	fitxer.open(nomFitxer);
-	if (fitxer.is_open())
-	{
-		if (m_Figura_a.getTipus() != NO_FIGURA)
-			m_tauler.colocarFigura(m_Figura_a);
+    ofstream fitxer;
+    fitxer.open(nomFitxer);
+    if (fitxer.is_open())
+    {
+        if (m_Figura_a.getTipus() != NO_FIGURA)
+            m_Tauler.colocaFigura(m_Figura_a);
 
-		int tauler[MAX_FILA][MAX_COL];
+        int tauler[MAX_FILA][MAX_COL];
 
-		m_tauler.getValorsTauler(tauler);
-		for (int i = 0; i < MAX_FILA; i++)
-		{
-			for (int j = 0; j < MAX_COL; j++)
-			{
-				fitxer << int(tauler[i][j]) << " ";
-			}
-			fitxer << endl;
-		}
+        m_Tauler.getValorsTauler(tauler);
+        for (int i = 0; i < MAX_FILA; i++)
+        {
+            for (int j = 0; j < MAX_COL; j++)
+            {
+                fitxer << tauler[i][j] << " ";
+            }
+            fitxer << endl;
+        }
 
-		fitxer.close();
-	}
+        fitxer.close();
+    }
 }
--- Joc.h
+++ Joc.h
@@ -1,5 +1,6 @@
 #ifndef JOC_H
 #define JOC_H
+
 #include "Tauler.h"
 #include "Figura.h"
 #include <string>
@@ -8,16 +9,15 @@ using namespace std;
 class Joc
 {
 public:
-	Joc() {}
-	void inicialitza(const string& nomFitxer);
-	bool giraFigura(DireccioGir direccio);
-	bool mouFigura(int dirX);
-	int baixaFigura();
-	void escriuTauler(const string& nomFitxer);
+    void inicialitza(const string& nomFitxer);
+    bool giraFigura(DireccioGir direccio);
+    bool mouFigura(int direccio);
+    int baixaFigura();
+    void escriuTauler(const string& nomFitxer);
 
 private:
-	Tauler m_Tauler;
-	Figura m_Figura_a;
+    Tauler m_Tauler;
+    Figura m_Figura_a;
 };
 
 #endif
