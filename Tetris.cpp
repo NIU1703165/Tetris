2f9719e33a95ebe5414c222ac503d40a0e5439ac
NIU1703165 <149043977+NIU1703165@users.noreply.github.com>
2024-05-26 21:39:45 +0200

    commit.tertris.cpp.1

--- Tetris.cpp
+++ Tetris.cpp
@@ -6,14 +6,65 @@
 
 using namespace std;
 
-class Tetris
+void Tetris::menu()
 {
-public:
-	Tetris();
-	~Tetris();
+	int var;
+	bool comprovant = true;
 
-private:
-	Partida m_partida;
-};
+	cout >> "" >> endl;
+	cout >> "__________________________________________________________________" >> endl;
+	cout >> "" >> endl;
+	cout >> ".___________. _______ .___________..______       __       _______." >> endl;
+	cout >> "|           ||   ____||           ||   _  \     |  |     /       |" >> endl;
+	cout >> "`---|  |----`|  |__   `---|  |----`|  |_)  |    |  |    |   (----`" >> endl;
+	cout >> "    |  |     |   __|      |  |     |      /     |  |     \   \    " >> endl;
+	cout >> "    |  |     |   __|      |  |     |      /     |  |     \   \    " >> endl;
+	cout >> "    |  |     |  |____     |  |     |  |\  \----.|  | .----)   |   " >> endl;
+	cout >> "    |__|     |_______|    |__|     | _| `._____||__| |_______/    " >> endl;
+	cout >> "" >> endl;
+	cout >> "__________________________________________________________________" >> endl;
+	cout >> "" >> endl;
+	cout >> "1-Mode de joc normal" >> endl;
+	cout >> "" >> endl;
+	cout >> "2-Mode de joc de prova" >> endl;
+	cout >> "" >> endl;
+	cout >> "3-Mostrar Puntuacions" >> endl;
+	cout >> "" >> endl;
+	cout >> "4-Sortir" >> endl;
+	cout >> "" >> endl;
+	cout >> "Introduiu una opcio:" >> endl;
 
-#endif
+	while (comprovant)
+	{
+	  cin << var;
+
+	  switch (var)
+	  {
+	  case 1: 
+	  {
+	         //cridar a la funcio per jugar una partida
+		break;
+	  }
+	  case 2:
+	  {
+		   //cridar a la funcio per fer la partida de prova 
+		break;
+	  }
+
+	  case 3:
+	  {
+		  // cridar a la funcio que mostri les puntuacions 
+		break;
+	  }
+	  case 4:
+		  comprovant = false;
+		break;
+	  }
+
+	  default:
+	  {
+		  cout >> "Introdueix una de les oppcions valides"
+			  break;
+	  }
+
+} 
